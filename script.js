af0b9c8d40d6f77d147d578761548197790f78f8
Urielsousa <urielzinho@hotmail.com>
2025-11-05 17:36:11 -0300

    Add ENUM

--- script.js
+++ script.js
@@ -5,12 +5,22 @@ const machineScore = document.querySelector("#machine-score");
 let humanScoreNumber = 0;
 let machineScoreNumber = 0;
 
+const Game_OPTIONS = {
+  ROCK: "rock",
+  PAPER: "paper",
+  SCISSORS: "scissors",
+};
+
 const playHuman = (humanChoice) => {
   playTheGame(humanChoice, playMachine());
 };
 
 const playMachine = () => {
-  const choices = ["rock", "paper", "scissors"];
+  const choices = [
+    Game_OPTIONS.ROCK,
+    Game_OPTIONS.PAPER,
+    Game_OPTIONS.SCISSORS,
+  ];
   const randomNumber = Math.floor(Math.random() * 3);
   return choices[randomNumber];
 };
@@ -20,9 +30,12 @@ const playTheGame = (humanChoice, machineChoice) => {
   if (humanChoice === machineChoice) {
     result.innerHTML = "Deu Empate!";
   } else if (
-    (humanChoice === "paper" && machineChoice === "rock") ||
-    (humanChoice === "rock" && machineChoice === "scissors") ||
-    (humanChoice === "scissors" && machineChoice === "paper")
+    (humanChoice === Game_OPTIONS.PAPER &&
+      machineChoice === Game_OPTIONS.ROCK) ||
+    (humanChoice === Game_OPTIONS.ROCK &&
+      machineChoice === Game_OPTIONS.SCISSORS) ||
+    (humanChoice === Game_OPTIONS.SCISSORS &&
+      machineChoice === Game_OPTIONS.PAPER)
   ) {
     humanScoreNumber++;
     humanScore.innerHTML = humanScoreNumber;
